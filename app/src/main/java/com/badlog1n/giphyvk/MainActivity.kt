1fb6a2659a89176084a03ba4fe58c4ff485fb31b
Stepan Nemchenko <st-nemchenko@yandex.ru>
2023-03-05 13:25:30 +0300

    Проверка на доступ к интернету

--- app/src/main/java/com/badlog1n/giphyvk/MainActivity.kt
+++ app/src/main/java/com/badlog1n/giphyvk/MainActivity.kt
@@ -2,9 +2,12 @@ package com.badlog1n.giphyvk
 
 import android.app.DownloadManager
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.badlog1n.giphyvk.databinding.ActivityMainBinding
 
@@ -12,11 +15,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        if (!internetCheck()){
+            Toast.makeText(this, "Зайдите в приложение с доступом к интернету", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+        else{
+            val binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+        }
     }
 
+
+    fun internetCheck(): Boolean{
+        val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+        return activeNetwork?.isConnectedOrConnecting == true
+    }
     fun downloadGif(url: String, title: String) {
         val request = DownloadManager.Request(Uri.parse(url))
         request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
