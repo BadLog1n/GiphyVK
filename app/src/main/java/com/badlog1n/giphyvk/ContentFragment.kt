216f87751a16b8a0e0ea02e64c5b141a5e891e76
Stepan Nemchenko <st-nemchenko@yandex.ru>
2023-03-05 11:25:05 +0300

    Реализовано отображение крупной гифки

--- app/src/main/java/com/badlog1n/giphyvk/ContentFragment.kt
+++ app/src/main/java/com/badlog1n/giphyvk/ContentFragment.kt
@@ -1,23 +1,18 @@
 package com.badlog1n.giphyvk
 
-import android.content.*
-import android.graphics.BitmapFactory
-import android.net.Uri
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.badlog1n.giphyvk.databinding.FragmentContentBinding
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.net.URISyntaxException
 
 
 class ContentFragment : Fragment(), ContentPhotoAdapter.RecyclerViewEvent {
@@ -30,13 +25,8 @@ class ContentFragment : Fragment(), ContentPhotoAdapter.RecyclerViewEvent {
         return binding.root
 
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        //        bundle.putString("url", image)
-        //        bundle.putString("title", gifData?.title)
-        //        bundle.putString("name", gifData?.name)
-        //        bundle.putString("rating", gifData?.rating)
-        //        bundle.putString("time", gifData?.time)
-        //        bundle.putString("link", gifData?.link)
         val bundle = this.arguments
         if (bundle != null) {
             binding.title.text = bundle.getString("title")
@@ -44,6 +34,40 @@ class ContentFragment : Fragment(), ContentPhotoAdapter.RecyclerViewEvent {
             binding.rating.text = bundle.getString("rating")
             binding.time.text = bundle.getString("time")
             binding.link.text = bundle.getString("link")
+            CoroutineScope(Dispatchers.Main).launch {
+                try {
+                    binding.toImgProgress.visibility = View.VISIBLE
+
+                    Glide.with(binding.root)
+                        .load(bundle.getString("url"))
+                        .listener(object : RequestListener<Drawable> {
+                            override fun onLoadFailed(
+                                e: GlideException?,
+                                model: Any?,
+                                target: com.bumptech.glide.request.target.Target<Drawable>?,
+                                isFirstResource: Boolean
+                            ): Boolean {
+                                binding.toImgProgress.visibility = View.GONE
+                                return false
+                            }
+
+                            override fun onResourceReady(
+                                resource: Drawable?,
+                                model: Any?,
+                                target: com.bumptech.glide.request.target.Target<Drawable>?,
+                                dataSource: com.bumptech.glide.load.DataSource?,
+                                isFirstResource: Boolean
+                            ): Boolean {
+                                binding.toImgProgress.visibility = View.GONE
+                                return false
+                            }
+
+                        })
+                        .into(binding.mainImage)
+                } catch (e: Exception) {
+                    binding.toImgProgress.visibility = View.GONE
+                }
+            }
 /*            val imageRc: RecyclerView = view.findViewById(R.id.imagesRcView)
             imageRc.adapter = rcAdapter
             rcAdapter.recordsList = ArrayList()
